f71a05d9fb62494c7437df6531c4c4256084bdb9
Julien <julien@phareouest.fr>
2019-04-29 17:04:17 +0200

    corners

--- src/fr/phareouest/EditionScreen.java
+++ src/fr/phareouest/EditionScreen.java
@@ -45,16 +45,28 @@ public class EditionScreen extends MainScreen implements FieldChangeListener {
     private BasicEditField inputKey;
     private ButtonField boutValider; 
     private ButtonField boutAnnuler; 
-    private KeyManager keymanager;
+   // private KeyManager keymanager = new KeyManager();
+
     
     
 	Border myBorder = BorderFactory.createBitmapBorder(new XYEdges(20, 16, 27, 23),Bitmap.getBitmapResource("img/corners.png"));
+	
 
     
     public void fieldChanged(Field field, int context) {
     	if (field == boutValider) {
-        //ligne1.getText();
-    	Dialog.inform("Clé enregistrée !");
+    		KeyManager keymanager = new KeyManager();
+	        //ligne1.getText();
+	    	//keymanager.set(inputName.getText(),inputKey.getText());
+    		//keymanager = new KeyManager();
+    		//Object ik = inputKey.getText().trim();
+	    	//String keyValue = new String("5614561");
+    		String keyValue = new String(inputKey.getText().trim());
+    		Dialog.inform(keyValue);
+	    	//keymanager.set("alpha",keyValue);
+	    	//keymanager.commit();
+	    	//Dialog.inform("Clé enregistrée !");
+	    	getScreen().close();
     	}
     	if (field == boutAnnuler) {
     		getScreen().close();	
@@ -66,6 +78,7 @@ public class EditionScreen extends MainScreen implements FieldChangeListener {
     public EditionScreen() {
         super( MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR );
         setTitle("Edition");
+        KeyManager keymanager = new KeyManager();
         
         LabelField labName = new LabelField("Nom du Service :");
         labName.setPadding(10,10,10,10);
@@ -79,6 +92,8 @@ public class EditionScreen extends MainScreen implements FieldChangeListener {
         add(labKey);
         BasicEditField inputKey = new BasicEditField(TextField.NO_NEWLINE);
         inputKey.setBorder(myBorder);
+        String myValue = keymanager.get("alpha").toString();
+        inputKey.setText(myValue);
         add(inputKey);
         
         GridFieldManager EndLine = new GridFieldManager(1,3,Field.FIELD_VCENTER);
